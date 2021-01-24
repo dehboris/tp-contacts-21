09e577c2d70c6568eb0132e2ddfdc27f5e9f49d7
Prince Le Bon HOUNDJO <houndjoprincelebon3@gmail.com>
2021-01-24 22:27:37 +0100

    HOUNDJO Prince Le Bon

--- Contact/app/src/main/java/com/example/contact/MainActivity.java
+++ Contact/app/src/main/java/com/example/contact/MainActivity.java
@@ -8,7 +8,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.gson.Gson;
@@ -16,12 +15,10 @@ import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Collections;
-
 import model.Contact;
 import model.ContactAdapter;
 
-public class MainActivity extends AppCompatActivity implements Comparable<Contact> {
+public class MainActivity extends AppCompatActivity {
     private ArrayList<Contact> mContact;
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -108,9 +105,4 @@ public class MainActivity extends AppCompatActivity implements Comparable<Contac
         mRecyclerView.setAdapter(mAdapter);
     }
 
-
-    @Override
-    public int compareTo(Contact o) {
-        return 0;
-    }
 }
